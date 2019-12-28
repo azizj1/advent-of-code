afa076c3f4d30c316ebb7c56d040cf44feaaa7ab
Aziz Javed <Aziz.Javed@directsupply.com>
2019-12-27 18:24:40 -0600

    day 16 p1

---
+++ src/2019/16.ts
@@ -0,0 +1,50 @@
+import { getRunsFromIniFile } from '~/util/util';
+import input from './16.txt';
+import { timer } from '~/util/Timer';
+
+const getPattern = (repeatPerNumber: number, until: number) => {
+    const base = [0, 1, 0, -1] as const;
+    const result: typeof base[number][] = [];
+    let resultIndex = 0;
+    while (result.length < until) {
+        const baseIndex = resultIndex % base.length;
+        const offset = resultIndex === 0 ? 1 : 0;
+
+        for (let i = 0; i < repeatPerNumber - offset && result.length < until; i++) {
+            result.push(base[baseIndex]);
+        }
+        resultIndex++;
+    }
+    return result;
+};
+
+export const fft = (signal: number[], numOfPhases: number) => {
+    let output = [...signal];
+    for (let i = 0; i < numOfPhases; i++) {
+        const workingCopy: number[] = [];
+        for (let j = 1; j <= signal.length; j++) {
+            const pattern = getPattern(j, signal.length);
+            const sumProduct = output.reduce((a, c, i) => a + c * pattern[i], 0);
+            workingCopy.push(Math.abs(sumProduct % 10));
+        }
+        output = workingCopy;
+    }
+    return output.slice(0, 8);
+};
+
+export const getSignalsFromFile = () => getRunsFromIniFile(input).map(s => ({
+    name: s.name.split(',')[0],
+    numofPhases: Number(s.name.split(',')[1]),
+    signal: s.content.replace(/\r?\n/, '').split('').map(Number)
+}));
+
+export const run = () => {
+    const sims = getSignalsFromFile(); // .slice(0, -1);
+    for (const s of sims) {
+        console.log(timer.start(`16 - ${s.name}`));
+        console.log(fft(s.signal, s.numofPhases));
+        console.log(timer.stop());
+    }
+};
+
+run();
--- src/watch.ts
+++ src/watch.ts
@@ -1,6 +1,6 @@
 // it's dynamically loaded because it allows us to continue running if there is a runtime error
 try {
-    import(/* webpackChunkName: "problem1" */ './2019/15b');
+    import(/* webpackChunkName: "problem1" */ './2019/16');
 } catch (e) {
     console.log(e);
 }
