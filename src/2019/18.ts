4c6cb65c27065a045b215705d96f954800681f45
Aziz Javed <Aziz.Javed@directsupply.com>
2019-12-31 18:01:36 -0600

    18 wip

--- src/2019/18.ts
+++ src/2019/18.ts
@@ -1,7 +1,8 @@
 import { getRunsFromIniFile } from '~/util/util';
 import input from './18.txt';
-import { IPoint, toKey } from '~/2019/10';
+import { IPoint, toKey as toString } from '~/2019/10';
 import { timer } from '~/util/Timer';
+import { PriorityQueue } from '~/util/PriorityQueue';
 
 interface ITunnel {
     name: string;
@@ -41,9 +42,13 @@ export const getSimulations = () => getRunsFromIniFile(input)
     }))
     .map(toTunnel);
 
-const getNeighbors = ({row, col}: IPoint, exclude: IPoint | null): IPoint[] =>
+export const equals = (p1?: IPoint, p2?: IPoint) => p1 != null && p2 != null ?
+    p1.col === p2.col && p1.row === p2.row :
+    false;
+
+const getNeighbors = ({row, col}: IPoint, exclude?: IPoint | null): IPoint[] =>
     exclude == null ?
-        [{row: row - 1, col}, {row: row + 1, col}, {row, col: col -1}, {row, col: col + 1}] :
+        [{row: row - 1, col}, {row: row + 1, col}, {row, col: col + 1}, {row, col: col - 1}] :
         getNeighbors({row, col}, null).filter(p => p.row !== exclude.row || p.col !== exclude.col);
 
 export const collectKeys = ({grid, keys, entrance}: ITunnel) => {
@@ -51,11 +56,11 @@ export const collectKeys = ({grid, keys, entrance}: ITunnel) => {
     const get = ({row, col}: IPoint) => grid[row][col];
     const isValid = (p: IPoint, visited: Set<string>, keysObtained: Set<string>) => {
         const cell = get(p);
-        const pHash = toKey(p);
+        const pHash = toString(p);
         if (cell == null ||
             cell !== '.' &&
             cell !== '@' &&
-            toKey(keys.get(cell)) !== pHash && // stepping on a key is valid
+            toString(keys.get(cell)) !== pHash && // stepping on a key is valid
             !keysObtained.has(cell.toLowerCase()) && // stepping on A if 'a' is obtained is valid
             !visited.has(pHash)
         )
@@ -67,14 +72,16 @@ export const collectKeys = ({grid, keys, entrance}: ITunnel) => {
         if (!isValid(start, visited, keysObtained))
             return Infinity;
 
-        console.log('at ', toKey(start));
+        // if (from != null && from.col === entrance.col && from.row === entrance.row && keysObtained.size === 0) {
+        //     console.log('STARTING OVER');
+        // }
         const cell = get(start);
         let onKey = false;
 
-        visited.add(toKey(start));
-        if (keys.has(cell)) {
-            console.log('OBTAINED KEY: ', cell);
+        visited.add(toString(start));
+        if (keys.has(cell) && !keysObtained.has(cell)) {
             keysObtained.add(cell);
+            console.log('OBTAINED KEY: ', cell, '\n\tRemaining: ', Array.from(keys.keys()).filter(k => !keysObtained.has(k)));
             onKey = true;
             if (keysObtained.size === keys.size)
                 return 0;
@@ -84,24 +91,88 @@ export const collectKeys = ({grid, keys, entrance}: ITunnel) => {
         const neighbors = getNeighbors(start, onKey ? null : from);
         const steps: number[] = [];
         for (const neighbor of neighbors)
-            steps.push(helper(neighbor, start, onKey ? new Set() : visited, keysObtained));
+            steps.push(helper(neighbor, start, onKey ? new Set() : visited, new Set(keysObtained)));
 
-        if (steps.every(s => s === Infinity)) {
-            visited.delete(toKey(from));
-            
-        }
+        // console.log('backtracking at ', toKey(start));
         return 1 + Math.min(...steps);
     };
 
     return helper(entrance, null, new Set(), new Set());
 };
 
+export const getNearestKeysMap = ({grid, keys, doors, entrance}: ITunnel) => {
+    const result = new Map<string, PriorityQueue<{toKey: string; steps: number}>>();
+    const cache: {[toKey: string]: {[fromPoint: string]: number}} = {}; // cache[toKey][fromPoint] = steps;
+    for (const k of keys.keys()) {
+        cache[k] = {};
+        result.set(k, new PriorityQueue(k => k.steps));
+    }
+    result.set('@', new PriorityQueue(k => k.steps)); // need to add this for entrance
+
+    const get = ({row, col}: IPoint) => grid[row][col];
+    const isValid = (p: IPoint, visited: Set<string>) => {
+        const cell = get(p);
+        if (cell == null ||
+            cell !== '.' &&
+            cell !== '@' &&
+            !equals(keys.get(cell), p) && // stepping on a key is valid
+            !equals(doors.get(cell), p) && // stepping on a door is valid. We don't care about doors right now
+            !visited.has(toString(p))
+        )
+            return false;
+        return true;
+    };
+    const helper = (start: IPoint, visited: Set<string>, toKey: string): number => {
+        if (!isValid(start, visited))
+            return Infinity;
+        console.log('at', start, 'looking for', toKey);
+        const cell = get(start);
+        const pointStr = toString(start);
+        if (cache[toKey][pointStr] != null) {
+            console.log('HIT CACHE! from:', start, 'to:', toKey, 'steps:', cache[toKey][pointStr]);
+            return cache[toKey][pointStr];
+        }
+        if (cell === toKey)
+            return 0;
+
+        visited.add(pointStr);
+        const neighbors = getNeighbors(start).filter(n => !visited.has(toString(n)));
+        const steps: number[] = [];
+        for (const neighbor of neighbors)
+            steps.push(helper(neighbor, visited, toKey));
+
+        const minSteps = 1 + Math.min(...steps);
+        cache[toKey][pointStr] = minSteps;
+        console.log('put', cache[toKey][pointStr], 'into cache for toKey =', toKey, 'point =', pointStr);
+        return minSteps;
+    };
+
+    for (const k of keys.keys())
+        result.set('@', result.get('@')!.insert({
+            toKey: k,
+            steps: helper(entrance, new Set(), k)
+        }));
+    for (const fromKey of keys.keys())
+        for (const toKey of keys.keys()) {
+            if (fromKey === toKey)
+                continue;
+            const steps = helper(keys.get(fromKey)!, new Set(), toKey);
+            // if fromKey to toKey is 5 steps, then toKey to fromKey is 5 steps.
+            cache[fromKey][toString(keys.get(toKey))] = steps;
+            result.set(fromKey, result.get(fromKey)!.insert({
+                toKey,
+                steps
+            }));
+        }
+    return result;
+};
+
 export const run = () => {
-    const sims = getSimulations().slice(1, 2);
+    const sims = getSimulations().slice(0, 1);
     for (const s of sims) {
         console.log(timer.start(`18 - ${s.name}`));
         console.log(s.grid.map(r => r.join(' ')).join('\n'));
-        console.log(collectKeys(s));
+        console.log(Array.from(getNearestKeysMap(s).entries()).reduce((m, [k, v]) => m.set(k, v.toString()), new Map<string, {toKey: string; steps: number}[]>()));
         console.log(timer.stop());
     }
 };
---
+++ src/util/PriorityQueue.ts
@@ -0,0 +1,135 @@
+/*
+    Since complete binary heaps handle insertion level-by-level, we can just use an array to
+    to hold our data, and use a level-ordered algorithm to dictate how each point is stored
+    E.g., if we have the heap
+        50
+        / \
+      19   4
+      / \
+     3   6
+    it'll be represented as [50, 19, 4, 3, 6]
+
+    REMEMBER: Heaps are not binary trees. The only rule for heaps is that both children of a node must be less than
+    the node itself, so if it's just [50, 19], and we're about to add a 4, it'll become [50, 19, 4], which is
+        50
+        / \
+      19   4
+    and now if we add the element 3, it'll become [50, 19, 4, 3], and represented as
+        50
+        / \
+      19   4
+      /
+     3
+
+     What happens if we were to insert 36, though? We push it to the array, making it [50, 19, 4, 3, 36], and then
+     compare 36 to its parent, which is Math.floor(x / 2), where x = new element = arr.length - 1. All we do is swap!
+     And we get [50, 36, 4, 3, 19]
+*/
+
+class Node<T> {
+    public val: T;
+    public priority: number;
+
+    constructor(item: T, priority: number) {
+        this.val = item;
+        this.priority = priority;
+    }
+}
+
+type PriorityFunction<T> = (item: T) => number;
+
+// This is s MAX HEAP Priority Queue. Highest priority is at front, and what will be retrieved first
+// To make it a MIN heap, just multiply priority by -1 before adding it to queue.
+export class PriorityQueue<T> {
+    // first element is null, rest is type of Node<T>. Why is the first element null? See poll()
+    private heap: [null, ...Array<Node<T>>];
+    private comparator: PriorityFunction<T>;
+
+    constructor(comparator: PriorityFunction<T>) {
+        this.heap = [null];
+        this.comparator = comparator;
+    }
+
+    // O(log N)
+    insert(item: T) {
+        const priority = this.comparator(item);
+        const node = new Node(item, priority);
+        this.heap.push(node);
+        let nodeIndex = this.heap.length - 1;
+        let parentIndex = Math.floor(nodeIndex / 2);
+
+        while (this.heap[parentIndex] != null && this.heap[parentIndex]!.priority < priority) {
+            this.swap(nodeIndex, parentIndex);
+            nodeIndex = parentIndex;
+            parentIndex = Math.floor(nodeIndex / 2);
+        }
+        return this;
+    }
+
+    // O(log N)
+    // this is removal of the highest priority, which is always the first element in the array after the null
+    // we can't just do shift() because if we have something like this: [50, 40, 90], it'll become [40, 90], and 90
+    // is greater. Plus, that's a O(N) operation. We can do O(log N)
+    // All we do is 1) swap last element with first element. 2) Pop last element out. 3) since it's likely that
+    // the the first node's children are larger now, we go down the tree, swapping elements as we progress.
+    // We swap parentX with Math.max(leftRight, rightChild). I.e., if front node is at index X, we check 2X and 2X + 1
+    // which is where X's children will be.
+    // This also explains why the first element is NULL. With the first element starting at index = 1, we can find its
+    // children by doing 2*1 and 2*1 + 1. If first element was at index = 0, we'd be screwed, because 2*0 = 0
+    poll() {
+        if (this.heap.length <= 2) { // i.e, there is only 1 element. [null, singleElement]
+            const removedElement = this.heap.pop();
+            this.heap[0] = null;
+            return removedElement == null ? removedElement : removedElement.val;
+        }
+        this.swap(1, this.heap.length - 1);
+        const removedElement = this.heap.pop();
+        let nodeIndex = 1;
+        let higherPriorityChildIndex = this.getMaxChildIndex(nodeIndex);
+        while (
+            this.heap[higherPriorityChildIndex] != null &&
+            this.heap[higherPriorityChildIndex]!.priority >= this.heap[nodeIndex]!.priority
+        ) {
+            this.swap(higherPriorityChildIndex, nodeIndex);
+            nodeIndex = higherPriorityChildIndex;
+            higherPriorityChildIndex = this.getMaxChildIndex(nodeIndex);
+        }
+        return removedElement!.val;
+    }
+
+    peek() {
+        return this.heap[1] != null ? this.heap[1].val : null;
+    }
+
+    toArray() {
+        return this.heap.slice(1).map(m => m!.val);
+    }
+
+    isEmpty() {
+        return this.heap.length <= 1;
+    }
+
+    size() {
+        return this.heap.length - 1;
+    }
+
+    toString() {
+        return this.heap.slice(1).map(n => n!.val);
+    }
+
+    private swap(indexA: number, indexB: number) {
+        const temp = this.heap[indexA];
+        this.heap[indexA] = this.heap[indexB];
+        this.heap[indexB] = temp;
+    }
+
+    private getMaxChildIndex(index: number) {
+        const leftChildIndex = 2 * index;
+        const rightChildIndex = 2 * index + 1;
+        if (rightChildIndex >= this.heap.length || leftChildIndex >= this.heap.length)
+            return leftChildIndex;
+        return this.heap[leftChildIndex]!.priority > this.heap[rightChildIndex]!.priority ?
+            leftChildIndex :
+            rightChildIndex;
+    }
+}
