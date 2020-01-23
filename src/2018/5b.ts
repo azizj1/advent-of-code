13d2d15ad3803b9c2ae230e152b1c5fe5e3979b1
Aziz Javed <azizjaved0@gmail.com>
2020-01-23 09:03:47 -0600

    2018 day 5 more notes

--- src/2018/5b.ts
+++ src/2018/5b.ts
@@ -3,27 +3,29 @@ import { timer } from '~/util/Timer';
 import { last, declareProblem } from '~/util/util';
 
 const react = (polymer: string) => (exclude: string) => {
-    const result = [''];
+    const result: string[] = [];
     let index = 0;
     while (index < polymer.length) {
         const char = polymer.charAt(index);
-        if (willReact(last(result), char))
+        if (willReact(last(result) ?? '', char))
             result.pop();
         else if (char.toUpperCase() !== exclude)
             result.push(char);
         index++;
     }
-    return result.length - 1;
+    return result;
 };
 
 const reactWithImprovements = (polymer: string) => {
     const begin = 'A'.charCodeAt(0);
     const end = 'Z'.charCodeAt(0);
     let shortestPolymer = Infinity;
+
+    const smallerPolymer = react(polymer)('').join('');
     for (let i = begin; i <= end; i++)
         shortestPolymer = Math.min(
             shortestPolymer,
-            react(polymer)(String.fromCharCode(i))
+            react(smallerPolymer)(String.fromCharCode(i)).length
         );
     return shortestPolymer;
 };
