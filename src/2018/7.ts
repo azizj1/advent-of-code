eac61e3ad13878f2dc97e8c5bd18c18ccda5f9db
Aziz Javed <azizjaved0@gmail.com>
2020-02-07 15:36:11 -0600

    2018 day 7 wip

--- src/2018/7.ts
+++ src/2018/7.ts
@@ -24,8 +24,29 @@ export const getSimulations = (): ISimulation[] => getRunsFromIniNewlineSep(inpu
         };
     });
 
+const printGraph = (graph: Graph<string>) => {
+    const vertices = Array.from(graph.vertices).sort((a, b) => a.localeCompare(b));
+    vertices.forEach(v => {
+        console.log(`${v} -> ${Array.from(graph.edges.get(v) ?? new Set<string>()).sort((a, b) => a.localeCompare(b))}`);
+    });
+    console.log('============');
+};
+
+// export const topologicalSort2 = ({graph}: ISimulation) => {
+//     const visited = new Set<string>();
+//     const finalResult: string[] = [];
+
+//     const helper = (startNode: string): string[] => {
+//         if (visited.has(startNode))
+//             return [];
+//         visited.add(startNode);
+
+
+//     };
+// };
+
 export const topologicalSort = ({graph}: ISimulation) => {
-    console.log(graph.toString());
+    printGraph(graph);
     const visited = new Set();
     const stack: string[] = [];
     const finalResult: string[] = [];
@@ -41,7 +62,13 @@ export const topologicalSort = ({graph}: ISimulation) => {
 
         while (stack.length > 0) {
             const curr = last(stack);
-            const adjList = graph.edges.get(curr) ?? new Set();
+            const adjList = Array.from(graph.edges.get(curr) ?? new Set<string>());
+            adjList.sort((a, b) => b.localeCompare(a)); // we want the last one to be first
+            if (adjList.filter(a => !visited.has(a)).length === 0) {
+                result.push(stack.pop()!);
+                continue;
+            }
+            console.log(`at ${curr} -> ${adjList.join(',')}`);
 
             for (const adj of adjList) {
                 if (visited.has(adj))
@@ -49,12 +76,15 @@ export const topologicalSort = ({graph}: ISimulation) => {
                 visited.add(adj);
                 stack.push(adj);
             }
-            result.push(stack.pop()!);
         }
+        console.log('-------------');
+        console.log(result);
+        console.log('-------------');
         return result;
     };
 
-    for (const v of Array.from(graph.vertices).sort((a, b) => a.localeCompare(b)))
+    const startingVertices = Array.from(graph.vertices).filter(v => graph.edges.get(v) == null).sort((a, b) => a.localeCompare(b));
+    for (const v of startingVertices)
         finalResult.push(...helper(v));
     return finalResult.join('');
 };
@@ -65,7 +95,7 @@ export const runAg = (ag: (s: ISimulation) => string, title: string) => {
     for (const s of sims) {
         console.log(timer.start(`name=${s.name}, edges=${JSON.stringify(s.graph.size)}`));
         console.log(ag(s));
-        console.log(s.graph.getToplogicalSort().join(''));
+        // console.log(s.graph.getToplogicalSort().join(''));
         console.log(timer.stop());
     }
 };
