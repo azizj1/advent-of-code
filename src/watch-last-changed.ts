6ea14cb65a3fdb19f37c00fc9f310f1ccc176b6c
Aziz Javed <azizjaved0@gmail.com>
2020-09-16 16:08:01 -0500

    Fix typo

--- src/watch-last-changed.ts
+++ src/watch-last-changed.ts
@@ -16,7 +16,7 @@ export async function runLastChanged() {
     const dir = '2018';
     const file = await getLatestFile(dir, await getFiles(dir));
     const sanitized = file.replace(/(src)|(\.ts)/g, '');
-    // dir is hard-coded again below because without, dynamic improt will build every file that ends with *.ts
+    // dir is hard-coded again below because without, dynamic import will build every file that ends with *.ts
     // with it being hard-coded, it'll only build files that satisfy ./2018/*.ts
     try {
         const p = await import(/* webpackChunkName: "customProblem" */`./2018/${sanitized}.ts`);
