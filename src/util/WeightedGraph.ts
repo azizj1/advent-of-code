b74ce8598dda00a8647c097e84e31dc4adfe8f6b
Aziz Javed <azizjaved0@gmail.com>
2021-07-05 21:26:24 -0500

    Add tranpose method to WeightedGraph

--- src/util/WeightedGraph.ts
+++ src/util/WeightedGraph.ts
@@ -1,78 +1,92 @@
 export interface IPrintable {
-    toString(): string;
+  toString(): string;
 }
 
 export class WGraph<T extends IPrintable, E> {
-    private _vertices: Set<T>;
-    private edges: Map<T, Set<T>>;
-    private weights: Map<string, E>;
+  private _vertices: Set<T>;
+  private edges: Map<T, Set<T>>;
+  private weights: Map<string, E>;
 
-    constructor() {
-        this._vertices = new Set();
-        this.edges = new Map();
-        this.weights = new Map();
+  constructor() {
+    this._vertices = new Set();
+    this.edges = new Map();
+    this.weights = new Map();
+  }
+
+  addUndirectedEdge(from: T, to: T, weight?: E) {
+    this.addDirectedEdge(from, to, weight);
+    this.addDirectedEdge(to, from, weight);
+  }
+
+  addDirectedEdge(from: T, to: T, weight?: E) {
+    // because it's a set, if it already exists, it won't do anything.
+    this._vertices.add(from);
+    this._vertices.add(to);
+
+    if (!this.edges.has(from)) this.edges.set(from, new Set());
+
+    this.edges.get(from)!.add(to);
+
+    if (weight != null)
+      this.weights.set(`${from.toString()},${to.toString()}`, weight);
+  }
+
+  addVertex(node: T) {
+    this._vertices.add(node);
+    if (!this.edges.has(node)) this.edges.set(node, new Set());
+  }
+
+  hasEdge(from: T, to: T) {
+    return this.edges.has(from) && this.edges.get(from)!.has(to);
+  }
+
+  getWeight(from: T, to: T) {
+    return this.weights.get(`${from.toString()},${to.toString()}`);
+  }
+
+  setWeight(from: T, to: T) {
+    if (!this.hasEdge(from, to))
+      throw new Error(
+        `Cannot add weight to path ${from.toString()} -> ${to.toString()} because it doesn't exist`
+      );
+  }
+
+  toString(wToString?: (w: E | undefined) => string) {
+    let str = '';
+    if (wToString != null) {
+      for (const [key, val] of this.edges.entries())
+        str += `${key} -> ${Array.from(val)
+          .map((v) => `${wToString(this.getWeight(key, v))} ${v}`)
+          .join(', ')}\n`;
+    } else {
+      for (const [key, val] of this.edges.entries())
+        str += `${key} -> ${Array.from(val).join(',')}\n`;
     }
+    return str;
+  }
 
-    addUndirectedEdge(from: T, to: T, weight?: E) {
-        this.addDirectedEdge(from, to, weight);
-        this.addDirectedEdge(to, from, weight);
+  getAdjList(v: T) {
+    return Array.from(this.edges.get(v) ?? []);
+  }
+
+  get vertices() {
+    return Array.from(this._vertices.values());
+  }
+
+  tranpose() {
+    const graph = new WGraph<T, E>();
+    for (const v of this.vertices) {
+      graph.addVertex(v);
     }
-
-    addDirectedEdge(from: T, to: T, weight?: E) {
-        // because it's a set, if it already exists, it won't do anything.
-        this._vertices.add(from);
-        this._vertices.add(to);
-
-        if (!this.edges.has(from))
-            this.edges.set(from, new Set());
-
-        this.edges.get(from)!.add(to);
-
-        if (weight != null)
-            this.weights.set(`${from.toString()},${to.toString()}`, weight);
+    for (const [edge, connections] of this.edges) {
+      for (const connection of connections) {
+        graph.addDirectedEdge(
+          connection,
+          edge,
+          this.getWeight(edge, connection)
+        );
+      }
     }
-
-    addVertex(node: T) {
-        this._vertices.add(node);
-        if (!this.edges.has(node))
-            this.edges.set(node, new Set());
-    }
-
-    hasEdge(from: T, to: T) {
-        return this.edges.has(from) && this.edges.get(from)!.has(to);
-    }
-
-    getWeight(from: T, to: T) {
-        return this.weights.get(`${from.toString()},${to.toString()}`);
-    }
-
-    setWeight(from: T, to: T) {
-        if (!this.hasEdge(from, to))
-            throw new Error(
-                `Cannot add weight to path ${from.toString()} -> ${to.toString()} because it doesn't exist`
-            );
-    }
-
-    toString(wToString?: (w: E | undefined) => string) {
-        let str = '';
-        if (wToString != null) {
-            for (const [key, val] of this.edges.entries())
-                str += `${key} -> ${Array.from(val)
-                        .map(v => `${wToString(this.getWeight(key, v))} ${v}`)
-                        .join(', ')}\n`;
-        }
-        else {
-            for (const [key, val] of this.edges.entries())
-                str += `${key} -> ${Array.from(val).join(',')}\n`;
-        }
-        return str;
-    }
-
-    getAdjList(v: T) {
-        return Array.from(this.edges.get(v) ?? []);
-    }
-
-    get vertices() {
-        return Array.from(this._vertices.values());
-    }
+    return graph;
+  }
 }
