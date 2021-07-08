848dea835d6064e46b52a41e447bc24bdac116e1
Aziz Javed <azizjaved0@gmail.com>
2021-07-08 16:13:38 -0500

    2020 day 10a

--- src/2020/10.ts
+++ src/2020/10.ts
@@ -1,16 +1,47 @@
 import { timer } from '~/util/Timer';
-import { getRunsFromIniNewlineSep } from '~/util/util';
+import { getRunsFromIniNewlineSep, pipe } from '~/util/util';
 import input from './10.txt';
 
 interface Simulation {
   name: string;
+  adapters: number[];
 }
 
 function getSimulations(): Simulation[] {
-  throw new Error('Not implemented.');
+  return getRunsFromIniNewlineSep(input).map((sim) => ({
+    name: sim.name,
+    adapters: sim.content.map(Number),
+  }));
+}
+
+function getDistributionOfJoltDifferences({ adapters }: Simulation): number[] {
+  const distributions = [...adapters]
+    .sort((a, b) => a - b)
+    .reduce((distr, adapter, idx, sortedAdapters) => {
+      const prev = sortedAdapters[idx - 1] ?? 0;
+      if (distr[adapter - prev]) {
+        distr[adapter - prev]++;
+      } else {
+        distr[adapter - prev] = 1;
+      }
+      return distr;
+    }, [] as number[]);
+
+  // your device is rated 3 jolts higher than max, so including that, your
+  // 3-diff goes up by 1.
+  distributions[3]++;
+  return distributions;
+}
+
+function multiply(distr: number[]): number {
+  return distr.filter((d) => !isNaN(d)).reduce((agg, curr) => curr * agg);
 }
 
 export function run() {
-  const sim = getSimulations()[0];
-  // TODO(azizj1)
+  const sim = getSimulations()[2];
+  timer.run(
+    pipe(getDistributionOfJoltDifferences, multiply),
+    `day 10 - ${sim.name}`,
+    sim
+  );
 }
