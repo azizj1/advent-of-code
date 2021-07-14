2dc41c542836b3371c2cf4dafc0a0ae8860557ae
Aziz Javed <azizjaved0@gmail.com>
2021-07-14 16:19:00 -0500

    Add ability to run ad-hoc tests

--- src/watch-last-changed.ts
+++ src/watch-last-changed.ts
@@ -1,32 +1,41 @@
 import { promises as fs } from 'fs';
 
 async function getFiles(dir: string) {
-    return (await fs.readdir(`src/${dir}`, { withFileTypes: true }))
-        .filter((f) => f.isFile() && f.name.slice(-3) === '.ts')
-        .map((f) => `${f.name}`);
+  return (await fs.readdir(`src/${dir}`, { withFileTypes: true }))
+    .filter((f) => f.isFile() && f.name.slice(-3) === '.ts')
+    .map((f) => `${f.name}`);
 }
 
 async function getLatestFile(dir: string, files: string[]) {
-    const times = await Promise.all(files.map((f) => fs.stat(`src/${dir}/${f}`)));
-    const filesWithTime = files.map((f, i) => ({
-        file: f,
-        time: times[i].mtime.getTime(),
-    }));
-    return filesWithTime.reduce((max, c) => (c.time > max.time ? c : max), filesWithTime[0]).file;
+  const times = await Promise.all(files.map((f) => fs.stat(`src/${dir}/${f}`)));
+  const filesWithTime = files.map((f, i) => ({
+    file: f,
+    time: times[i].mtime.getTime(),
+  }));
+  return filesWithTime.reduce(
+    (max, c) => (c.time > max.time ? c : max),
+    filesWithTime[0]
+  ).file;
 }
 
 export async function runLastChanged() {
-    const dir = '2020';
-    const file = await getLatestFile(dir, await getFiles(dir));
-    const sanitized = file.replace(/(src)|(\.ts)/g, '');
-    // dir is hard-coded again below because without, dynamic import will build every file that ends with *.ts
-    // with it being hard-coded, it'll only build files that satisfy ./2018/*.ts
-    try {
-        const p = await import(/* webpackChunkName: "customProblem" */ `./2020/${sanitized}.ts`);
-        p.run();
-    } catch (e) {
-        console.log(e);
+  const dir = '2020';
+  const file = await getLatestFile(dir, await getFiles(dir));
+  const sanitized = file.replace(/(src)|(\.ts)/g, '');
+  // dir is hard-coded again below because without, dynamic import will build every file that ends with *.ts
+  // with it being hard-coded, it'll only build files that satisfy ./2018/*.ts
+  try {
+    const p = await import(
+      /* webpackChunkName: "customProblem" */ `./2020/${sanitized}.ts`
+    );
+    if (typeof p.test === 'function') {
+      p.test();
     }
+    // always execute the run function.
+    p.run();
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 runLastChanged();
