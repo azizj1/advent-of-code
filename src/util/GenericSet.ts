cd7c6199c29d9ff150296b3267891fd7abc737bb
Aziz Javed <azizjaved0@gmail.com>
2022-12-15 11:26:43 -0600

    Allow enumeration of generic set

--- src/util/GenericSet.ts
+++ src/util/GenericSet.ts
@@ -1,59 +1,74 @@
 export type HashFunction<E> = (e: E) => string | number;
 
 export class GenericSet<E> extends Set<E | string | number> {
-    protected getHash: HashFunction<E>;
+  protected getHash: HashFunction<E>;
+  private valuesMap = new Map<E | string | number, E>();
 
-    /* eslint-disable no-dupe-class-members */
-    constructor(toClone: GenericSet<E>);
-    constructor(getHash: HashFunction<E>);
-    constructor(getHash: HashFunction<E>, data: E[] | IterableIterator<E>);
-    constructor(setOrHash: GenericSet<E> | HashFunction<E>, data?: E[] | IterableIterator<E>) {
-        super();
-        if (typeof setOrHash === 'function') {
-            this.getHash = setOrHash;
-            for (const d of data ?? [])
-                this.add(d);
-        }
-        else {
-            this.getHash = setOrHash.getHash;
-            for (const d of setOrHash)
-                super.add(d);
-        }
+  /* eslint-disable no-dupe-class-members */
+  constructor(toClone: GenericSet<E>);
+  constructor(getHash: HashFunction<E>);
+  constructor(getHash: HashFunction<E>, data: E[] | IterableIterator<E>);
+  constructor(
+    setOrHash: GenericSet<E> | HashFunction<E>,
+    data?: E[] | IterableIterator<E>
+  ) {
+    super();
+    if (typeof setOrHash === 'function') {
+      this.getHash = setOrHash;
+      for (const d of data ?? []) this.add(d);
+    } else {
+      this.getHash = setOrHash.getHash;
+      for (const d of setOrHash) super.add(d);
     }
-    /* eslint-enable no-dupe-class-members */
+  }
+  /* eslint-enable no-dupe-class-members */
 
-    add(e: E) {
-        super.add(this.getHash(e));
-        return this;
-    }
+  override add(e: E) {
+    const hash = this.getHash(e);
+    super.add(hash);
+    this.valuesMap.set(hash, e);
+    return this;
+  }
 
-    delete(e: E) {
-        return super.delete(this.getHash(e));
-    }
+  override delete(e: E) {
+    const hash = this.getHash(e);
+    const deleted = super.delete(hash);
+    this.valuesMap.delete(hash);
+    return deleted;
+  }
 
-    has(e: E) {
-        return super.has(this.getHash(e));
-    }
+  override has(e: E) {
+    return super.has(this.getHash(e));
+  }
 
-    hasHash(h: string | number) {
-        return super.has(h);
-    }
+  hasHash(h: string | number) {
+    return super.has(h);
+  }
 
-    equals(other: GenericSet<E>) {
-        if (this.size !== other.size)
-            return false;
-        for (const item of other.values())
-            if (!super.has(item))
-                return false;
-        return true;
-    }
+  equals(other: GenericSet<E>) {
+    if (this.size !== other.size) return false;
+    for (const item of other.values()) if (!super.has(item)) return false;
+    return true;
+  }
 
-    subsetOf(parent: GenericSet<E>, hashMapperOnSubset?: (i: string | number) => string | number) {
-        if (hashMapperOnSubset == null)
-            hashMapperOnSubset = i => i;
-        for (const item of this.values())
-            if (!parent.hasHash(hashMapperOnSubset(item as string | number) as string | number))
-                return false;
-        return true;
+  subsetOf(
+    parent: GenericSet<E>,
+    hashMapperOnSubset?: (i: string | number) => string | number
+  ) {
+    if (hashMapperOnSubset == null) hashMapperOnSubset = (i) => i;
+    for (const item of this.values())
+      if (
+        !parent.hasHash(
+          hashMapperOnSubset(item as string | number) as string | number
+        )
+      )
+        return false;
+    return true;
+  }
+
+  override *[Symbol.iterator](): IterableIterator<E> {
+    for (const hash of this.values()) {
+      yield this.valuesMap.get(hash)!;
     }
+  }
 }
