f31b50b7338c382e263f0cf78b808923744d395f
Aziz Javed <azizjaved0@gmail.com>
2022-12-20 11:14:41 -0600

    Move combinations function to util

--- src/util/math.ts
+++ src/util/math.ts
@@ -5,3 +5,35 @@ export function mod(a: number, m: number) {
   }
   return ans;
 }
+
+/*
+Input: [
+    [a, b],
+    [c, d]
+    [e]
+]
+Output: [
+    [a, c, e],
+    [a, d, e],
+    [b, c, e],
+    [b, d, e]
+]
+*/
+export function combinations<T>(lists: T[][]) {
+  const solutions: T[][] = [];
+
+  const helper = (atKeyIndex: number, curr: T[]) => {
+    if (curr.length === lists.length) {
+      solutions.push([...curr]);
+      return;
+    }
+    for (let i = atKeyIndex; i < lists.length; i++)
+      for (let j = 0; j < lists[i].length; j++) {
+        curr.push(lists[i][j]);
+        helper(i + 1, curr);
+        curr.pop();
+      }
+  };
+  helper(0, []);
+  return solutions;
+}
