3a8cac1aaf226cc3cf563a496e6cf5d0f6d2968c
Aziz Javed <azizjaved0@gmail.com>
2022-12-27 16:20:55 -0600

    2022 day 15 notes

--- src/2022/15.ts
+++ src/2022/15.ts
@@ -48,7 +48,10 @@ function getSimulations(): Simulation[] {
   }));
 }
 
-function positionsWithoutBeacon({ yaxis, sensorReadings }: Simulation) {
+function positionsWithoutBeacon(
+  { sensorReadings }: Simulation,
+  yaxis: number
+): SortedSet<Range> {
   const set = new SortedSet<Range>(
     (r) => `${r.start}-${r.end}`,
     (a, b) => a.start - b.start
@@ -66,13 +69,20 @@ function positionsWithoutBeacon({ yaxis, sensorReadings }: Simulation) {
   return set;
 }
 
-function reduceRanges(ranges: SortedSet<Range>) {
+/**
+ * @param ranges Set of sorted ranges that are potentially overlapping.
+ * @return Ranges that are mutually exclusive.
+ * Example: [[1,2],[2,5],[7,9]] -> [[1,5],[7,9]]
+ *          [[1,6],[2,4]]       -> [[1,6]]
+ */
+function reduceRanges(ranges: SortedSet<Range>): Range[] {
   const sortedRanges = ranges.sortedValues;
   const reducedRanges = [sortedRanges[0]];
   for (let i = 1; i < sortedRanges.length; i++) {
     const { start: nstart, end: nend } = sortedRanges[i];
     const { start: pstart, end: pend } = last(reducedRanges)!;
     assert(nstart >= pstart, 'Range should have already been sorted.');
+    // If there's no overlap between prev and next, just push the next one.
     if (nstart > pend) {
       reducedRanges.push(sortedRanges[i]);
     } else if (nend <= pend) {
@@ -97,14 +107,140 @@ function countPointsInRange(ranges: Range[], { yaxis, beacons }: Simulation) {
   return count;
 }
 
+// part 2
+/**
+ * @param ranges Sorted ranges that are already reduced to the minimum ranges.
+ * @return The inverse of the ranges listed. E.g.,
+ *    * ranges = [{start: 20, end: 35}], bounds: {min: 0, max: 40}. It'll return
+ *        [{start: 0, end: 19}, {start: 36, end: 40}]
+ *    * ranges = [{start: 15, end: 20}, {start: 25, end: 30}], bounds: [0, 40]
+ *        [{start: 0, end: 14}, {start: 21, end: 24}, {start: 31, end: 40}]
+ *    * ranges = [{start: -5, end: 10}, {start: 35, end: 40}], bounds: [0, 40]
+ *        [{start: 11, end: 34}]
+ */
+function getComplement(
+  ranges: Range[],
+  bounds: { min: number; max: number }
+): Range[] {
+  const { min, max } = bounds;
+  ranges = ranges.filter((range) => range.end >= min && range.start < max);
+  if (ranges.length === 0) return [{ start: min, end: max }];
+  const inverseRanges: Range[] = [];
+  let start = ranges[0].start <= min ? ranges[0].end + 1 : 0;
+  for (let i = 0; i <= ranges.length; i++) {
+    // implies the end of the previous one was at max or greater.
+    if (start > max) break;
+    if (!ranges[i]) {
+      // There is no next one, and range[i].end had to be
+      inverseRanges.push({ start, end: max });
+    } else if (start >= ranges[i].start) {
+      // Happens if {start: 4, end: 5}, {start: 6, end: 8}
+      // There's no complement between 5 and 5, so continue.
+      start = ranges[i].end + 1;
+      continue;
+    } else {
+      // start has to be < max because of the filter.
+      const end = ranges[i].start - 1;
+      inverseRanges.push({ start, end });
+      start = ranges[i].end + 1;
+    }
+  }
+  return inverseRanges;
+}
+
+function findDistressedBeacon(sim: Simulation) {
+  const min = 0;
+  const { maxXY: max } = sim;
+  let coordinate = new Coordinate();
+  for (let y = 0; y <= max; y++) {
+    const ranges = reduceRanges(positionsWithoutBeacon(sim, y));
+    console.log(
+      y,
+      ranges,
+      ranges.reduce((sum, curr) => sum + curr.end - curr.start, 0)
+    );
+    const complement = getComplement(ranges, { min, max });
+    if (complement.length > 1) {
+      throw new Error('Multiple ranges found.');
+    } else if (complement.length === 1) {
+      console.log('FOUND IT!');
+      coordinate = new Coordinate(
+        assert(
+          complement[0].start,
+          (n) => n === complement[0].end,
+          'Range has multiple points on x-axis.'
+        ),
+        y
+      );
+    }
+  }
+  return coordinate;
+  // throw new Error('Distressed beacon not found.');
+}
+
 export function run() {
   declareProblem('2022 day 15');
-  const sims = getSimulations();
+  const sims = getSimulations().slice(0, 1);
   for (const sim of sims) {
     timer.run(() => {
-      const ranges = reduceRanges(positionsWithoutBeacon(sim));
+      const ranges = reduceRanges(positionsWithoutBeacon(sim, sim.yaxis));
       console.log(ranges);
       console.log(countPointsInRange(ranges, sim));
     }, sim.name);
+  }
+
+  declareProblem('2022 day15 part 2');
+  for (const sim of sims) {
+    timer.run(() => {
+      const beacon = findDistressedBeacon(sim);
+      const tuningFrequency = beacon.x * sim.maxXY + beacon.y;
+      console.log(beacon, tuningFrequency);
+    }, sim.name);
+  }
+}
+
+const TEST_COMPLEMENT_SCENARIOS = [
+  {
+    ranges: [{ start: 20, end: 35 }],
+    bounds: { min: 0, max: 40 },
+    expected: [
+      { start: 0, end: 19 },
+      { start: 36, end: 40 },
+    ],
+  },
+  {
+    ranges: [
+      { start: 15, end: 20 },
+      { start: 25, end: 30 },
+    ],
+    bounds: { min: 0, max: 40 },
+    expected: [
+      { start: 0, end: 14 },
+      { start: 21, end: 24 },
+      { start: 31, end: 40 },
+    ],
+  },
+  {
+    ranges: [
+      { start: -5, end: 10 },
+      { start: 35, end: 40 },
+    ],
+    bounds: { min: 0, max: 40 },
+    expected: [{ start: 11, end: 34 }],
+  },
+];
+
+export function test() {
+  declareProblem('2022 15 test');
+  for (const { ranges, bounds, expected } of TEST_COMPLEMENT_SCENARIOS) {
+    console.log(
+      'input',
+      ranges,
+      '\nactual',
+      getComplement(ranges, bounds),
+      '\nexpected',
+      expected,
+      '\n'
+    );
   }
 }
